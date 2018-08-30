a8e654c44997acb3b2ef368fbbdf932415ab0747
JaimeMatias <matias.raul.jaime@gmail.com>
2018-08-30 10:32:50 -0300

    Segunda Prueba intellij y git

--- src/holamundo.java
+++ src/holamundo.java
@@ -1,5 +1,5 @@
 public class holamundo {
     public static void main (String[] args){
-        System.out.println("HOLA MUNDO DESDE JAVA, Cambio");
+        System.out.println("HOLA MUNDO DESDE JAVA,branchs, Cambio");
     }
 }
